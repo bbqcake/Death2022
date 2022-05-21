30d5de8b2ef27b5c78cd87fa00201584cc3a21e5
bbqcake <tdarkture@gmail.com>
2022-05-21 04:14:32 +0200

    Added forward movement and not much else

--- Source/Death/Private/Characters/DCharacter.cpp
+++ Source/Death/Private/Characters/DCharacter.cpp
@@ -3,32 +3,86 @@
 
 #include "Characters/DCharacter.h"
 
-// Sets default values
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+
 ADCharacter::ADCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+ 	
 	PrimaryActorTick.bCanEverTick = true;
+	
+	// Camera and friends
+	CameraHelperComponent = CreateDefaultSubobject<USceneComponent>("CameraHelperComponent");
+	CameraHelperComponent->SetupAttachment(RootComponent);
+	
+	
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+	SpringArmComponent->SetupAttachment(CameraHelperComponent);
+	SpringArmComponent->SetRelativeRotation(FRotator(0.f, 10.f, 90.f)); // Camera rotated so moving forward moves us right
+	SpringArmComponent->bUsePawnControlRotation = false;
+	SpringArmComponent->bInheritPitch = false;
+	SpringArmComponent->bInheritRoll = false;
+	SpringArmComponent->bInheritYaw = false;
+	SpringArmComponent->bDoCollisionTest = false;
+	SpringArmComponent->bEnableCameraLag = true;
+	SpringArmComponent->CameraLagSpeed = 5.f;
+	SpringArmComponent->CameraLagMaxDistance = 100.f;	
+	
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComponent->SetupAttachment(SpringArmComponent);
 
+	FacingRightCamOffset = 500.f;
+	FacingLeftCamOffset = 500.f;
+
+	// lock the Y plane so we cant run into the wilds
+	GetCharacterMovement()->SetPlaneConstraintEnabled(true);
+	GetCharacterMovement()->SetPlaneConstraintFromVectors(FVector(1.f, 0.f, 0.f), FVector(0.f, 0.f, 1.f));
 }
 
-// Called when the game starts or when spawned
+
 void ADCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+	GetViewportSize();
+	InitialViewPortSize = ViewportSize;
+
+	CameraHelperComponent->SetRelativeLocation(FVector(FacingRightCamOffset, 0.f, 0.f), true);
 	
 }
 
-// Called every frame
+
 void ADCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	
 }
 
-// Called to bind functionality to input
+
 void ADCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ADCharacter::MoveForward);	
 }
 
+void ADCharacter::MoveForward(float Value)
+{
+	if (Controller != nullptr && Value != 0.f)
+	{		
+		const FRotator YawRotator(0.f, Controller->GetControlRotation().Yaw, 0.f); 
+		const FVector Direction(FRotationMatrix(YawRotator).GetUnitAxis(EAxis::X)); // Returns: vector of the axis ( points in the Yaw direction of our controller
+		AddMovementInput(Direction, Value); // Speed and acceleration is not handled here. That's handled in Character Movement component			
+	}
+}
+
+FVector2D ADCharacter::GetViewportSize()
+{
+	if (GEngine)
+	{
+		GEngine->GameViewport->GetViewportSize(ViewportSize);
+	}
+	
+	return ViewportSize;
+}
--- Source/Death/Public/Characters/DCharacter.h
+++ Source/Death/Public/Characters/DCharacter.h
@@ -6,6 +6,10 @@
 #include "GameFramework/Character.h"
 #include "DCharacter.generated.h"
 
+class UCameraComponent;
+class USpringArmComponent;
+class USceneComponent;
+
 UCLASS()
 class DEATH_API ADCharacter : public ACharacter
 {
@@ -21,7 +25,30 @@ public:
 protected:	
 	virtual void BeginPlay() override;
 
+	void MoveForward(float Value);
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Camera")
+	UCameraComponent* CameraComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Camera")
+	USpringArmComponent* SpringArmComponent;
+	/* Show more of right or left of screen depending on what way the player is facing/moving */
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Camera")
+	USceneComponent* CameraHelperComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float FacingRightCamOffset;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float FacingLeftCamOffset;
+	
 private:
+	
+	FVector2D InitialViewPortSize;
+	FVector2D ViewportSize;
+
+	FVector2D GetViewportSize();
+	//void AdjustCamera();
+	
 	
 
 	
